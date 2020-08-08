772970b5c9b2e626e88326b4bb4155e5522ec3d1
HiYanXL <2606469283@qq.com>
2020-08-08 17:14:26 +0800

    20200808

--- trains/src/test/java/com/yxl/trains/trains/TrainsApplicationTests.java
+++ trains/src/test/java/com/yxl/trains/trains/TrainsApplicationTests.java
@@ -1,13 +1,13 @@
 package com.yxl.trains.trains;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TrainsApplicationTests {
+public class TrainsApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
---
+++ trains/src/test/java/com/yxl/trains/trains/usercases/AnnotationTests.java
@@ -0,0 +1,11 @@
+package com.yxl.trains.trains.usercases;
+
+import com.yxl.trains.trains.TrainsApplicationTests;
+import org.junit.Test;
+
+public class AnnotationTests extends TrainsApplicationTests {
+    @Test
+    private void test(){
+
+    }
+}
